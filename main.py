f6d1a8592b4ab481fb9d35dc2f8cedbb94aec939
MrGodSmile <mrgodsmile@yandex.ru>
2023-05-15 14:41:17 +0300

    change currency

--- main.py
+++ main.py
@@ -32,22 +32,22 @@ with open('BASE_INTENTS.json', 'r') as jsn:
 
 
 class Currency():
-    DOLLAR_BUN = 'https://www.google.by/search?q=курс+доллара+&sxsrf=APwXEdfluDW-LRIG3J2YfnMV_D7XsxuX9A%3A1684086414892&source=hp&ei=jh5hZM-eM8aM9u8P9eyzyAk&iflsig=AOEireoAAAAAZGEsnhZVb1-dJhVLyB300sQf9lSQkzzM&ved=0ahUKEwjPnZzkrvX-AhVGhv0HHXX2DJkQ4dUDCAk&uact=5&oq=курс+доллара+&gs_lcp=Cgdnd3Mtd2l6EAMyCAgAEIAEELEDMggIABCABBCxAzILCAAQgAQQsQMQgwEyCwgAEIAEELEDEIMBMgsIABCABBCxAxCDATILCAAQgAQQsQMQgwEyCAgAEIAEELEDMgUIABCABDILCAAQgAQQsQMQgwEyBQgAEIAEOgcIIxDqAhAnOggIABCPARDqAjoICC4QjwEQ6gI6CwgAEIoFELEDEIMBOg4ILhCKBRCxAxCDARDUAjoOCAAQgAQQsQMQgwEQyQM6DggAEIAEELEDEIMBEJIDUMcJWJYyYOQ9aAJwAHgAgAGsAYgBzAqSAQQxMi4ymAEAoAEBsAEK&sclient=gws-wiz'
-    RUBLI_BUN= 'https://www.google.by/search?q=курс+к+рублю&sxsrf=APwXEdfSRn-Ru5TcGr4jcflN2FThlprndA%3A1684104904393&source=hp&ei=yGZhZJXvFISkjgb_y5DADg&iflsig=AOEireoAAAAAZGF02H8MD0xCLNoV-uedt4ccxDIozazv&ved=0ahUKEwiV89nU8_X-AhUEksMKHf8lBOgQ4dUDCAk&uact=5&oq=курс+к+рублю&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgcIIxDqAhAnOggIABCPARDqAjoHCCMQigUQJzoLCAAQgAQQsQMQgwE6CwgAEIoFELEDEIMBOg4ILhCABBCxAxCDARDUAjoICAAQgAQQsQM6DggAEIAEELEDEIMBEMkDOg4IABCABBCxAxCDARCSAzoICAAQgAQQyQM6CAgAEIAEEJIDOgcIABCABBAKOgoIABCABBBGEIICUKUTWPsmYNgvaAFwAHgAgAFJiAG3BpIBAjEymAEAoAEBsAEK&sclient=gws-wiz'
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
-    full_page_dollar = requests.get(DOLLAR_BUN, headers=headers)
-    soup_dollar = BeautifulSoup(full_page_dollar.content, 'html.parser')
-    full_page_rub = requests.get(DOLLAR_BUN, headers=headers)
-    soup_rub = BeautifulSoup(full_page_dollar.content, 'html.parser')
+    @classmethod
+    def currency_soup(cls, currency_url):
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
+        full_page = requests.get(currency_url, headers=headers)
+        return BeautifulSoup(full_page.content, 'html.parser')
 
     @classmethod
-    def currencyusd(cls):
-        convert = Currency.soup_dollar.findAll("span", {"class": "DFlfde SwHCTb", "data-precision": 2})
+    def currency_usd(cls):
+        DOLLAR_BUN = 'https://www.google.by/search?q=курс+доллара+&sxsrf=APwXEdfluDW-LRIG3J2YfnMV_D7XsxuX9A%3A1684086414892&source=hp&ei=jh5hZM-eM8aM9u8P9eyzyAk&iflsig=AOEireoAAAAAZGEsnhZVb1-dJhVLyB300sQf9lSQkzzM&ved=0ahUKEwjPnZzkrvX-AhVGhv0HHXX2DJkQ4dUDCAk&uact=5&oq=курс+доллара+&gs_lcp=Cgdnd3Mtd2l6EAMyCAgAEIAEELEDMggIABCABBCxAzILCAAQgAQQsQMQgwEyCwgAEIAEELEDEIMBMgsIABCABBCxAxCDATILCAAQgAQQsQMQgwEyCAgAEIAEELEDMgUIABCABDILCAAQgAQQsQMQgwEyBQgAEIAEOgcIIxDqAhAnOggIABCPARDqAjoICC4QjwEQ6gI6CwgAEIoFELEDEIMBOg4ILhCKBRCxAxCDARDUAjoOCAAQgAQQsQMQgwEQyQM6DggAEIAEELEDEIMBEJIDUMcJWJYyYOQ9aAJwAHgAgAGsAYgBzAqSAQQxMi4ymAEAoAEBsAEK&sclient=gws-wiz'
+        convert = Currency.currency_soup(DOLLAR_BUN).findAll("span", {"class": "DFlfde SwHCTb", "data-precision": 2})
         return convert[0].text
 
     @classmethod
-    def currencyrub(cls):
-        convert = Currency.soup_rub.findAll("span", {"class": "DFlfde SwHCTb", "data-precision": 3})
+    def currency_rub(cls):
+        RUB_BUN = 'https://www.google.by/search?q=курс+рубля&sxsrf=APwXEdfg26YNygnwvIva9NmX64eKvpkdQQ%3A1684148019264&source=hp&ei=Mw9iZMzvDIaF9u8P5PSPsAE&iflsig=AOEireoAAAAAZGIdQ4ZxDKL8tZppf6yFKlHHnxgzK_Di&oq=курс+&gs_lcp=Cgdnd3Mtd2l6EAEYADIHCCMQigUQJzIHCCMQigUQJzIHCCMQigUQJzILCAAQgAQQsQMQgwEyCwgAEIAEELEDEIMBMgsIABCABBCxAxCDATILCAAQgAQQsQMQgwEyBQgAEIAEMgsIABCABBCxAxCDATIFCAAQgAQ6BwgjEOoCECc6CwgAEIoFELEDEIMBOg4ILhCABBCxAxCDARDUAjoFCC4QgAQ6CAguEIAEENQCOggILhDUAhCABDoKCAAQgAQQyQMQCjoICAAQigUQkgM6CAgAEIAEELEDOg4IABCABBCxAxCDARDJAzoOCAAQgAQQsQMQgwEQkgNQb1iZDGDBFWgBcAB4AYABwgGIAZwFkgEDNi4xmAEAoAEBsAEK&sclient=gws-wiz'
+        convert = Currency.currency_soup(RUB_BUN).findAll("span", {"class": "DFlfde SwHCTb", "data-precision": 3})
         return convert[0].text
 
 
@@ -377,20 +377,21 @@ class Assistant(QtWidgets.QMainWindow, interface.Ui_MainWindow, threading.Thread
             self.talk(f"Файл {task} уже существует")
 
     def currency(self):
-        choiceusd = ['к доллару', 'доллару', 'давай доллару']
-        choicerub = ['к рублю', 'рублю', 'к российскому рублю']
+        c = Currency()
+        choice_usd = ['к доллару', 'доллару', 'давай доллару']
+        choice_rub = ['к рублю', 'рублю', 'к российскому рублю', 'российскому рублю']
+        choice_exit = ['не хочу', 'больше не хочу узнавать', 'хватит', 'не надо', 'я передумал']
         self.talk("К какой валюте хотите узнать курс? (к доллару или к российскому рублю?)")
         choice = self.listen()
-
-        c = Currency()
-
-        for i in choicerub:
+        for i in choice_exit:
             if i == choice:
-                self.talk(fr"1 российский рубль = {c.currencyrub()}")
-
-        for i in choiceusd:
+                self.talk("Ну как хотите")
+        for i in choice_rub:
             if i == choice:
-                self.talk(fr"1 доллар = {c.currencyusd()}")
+                self.talk(fr"1 российский рубль  {c.currency_rub()} белларусских рублей")
+        for i in choice_usd:
+            if i == choice:
+                self.talk(fr"1 доллар - {c.currency_usd()} беларусских рублей")
 
     def joke(self):
         link = requests.get('http://anekdotme.ru/random')
